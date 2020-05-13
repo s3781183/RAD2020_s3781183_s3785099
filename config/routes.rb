8af7909600206c4fc46e4d1b55c83392c9d868c0
Ruchelle Balasuriya <rushebala@gmail.com>
2020-05-13 17:41:00 +1000

    micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   resources :microposts
   get 'users/new'
   resources :users
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -26,6 +26,7 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "a" *51
     assert_not@user.valid?
   end
+
   test "email should not be too long" do
     @user.email="a"*244+"@example.com"
     assert_not @user.valid?
@@ -33,7 +34,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do
     valid_addresses=%w[user@example.com USER@foo.COM A_US-ER@foo.bar.orgfirst.last@foo.jp alice+bob@baz.cn]
-    valid_addresses.each do|valid_address|
+    valid_addresses.each do |valid_address|
       @user.email=valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
   
@@ -45,7 +46,20 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test"password should be present (nonblank)"do
+    @user.password=@user.password_confirmation=""*6
+    assert_not@user.valid?
+  end
+
+  test"password should have a minimum length"do
+    @user.password=@user.password_confirmation="a"*5
+    assert_not@user.valid?
+end 
+
+
+
   test "authenticated? should return false for a user with nil digest" do   
     assert_not @user.authenticated?('')   
   end 
+end
 end
