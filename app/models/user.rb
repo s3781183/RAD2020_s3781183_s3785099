942d5db3e8fa7216794be2734af31b62c7ffd779
Si-Long <syrialeong@gmail.com>
2020-05-13 06:55:53 +0000

    Login in done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
     before_save{self.email=email.downcase}
-    validates:name, presence:true, length:{maximum:50}
+    #validates:name, presence:false
     VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates:email,presence: true,length:{maximum:255}, format:{with:VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
     has_secure_password
     validates:password, presence: true, length: { minimum: 8 }
-    validates:mobile, presence: true, length: { minimum: 10 }
+    validates:mobile, presence: true, length: { minimum: 10 }, length: { maximum: 13 }
 
     def self.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  root 'users#new'
   resources :microposts
   get 'users/new'
   resources :users
+  get'/login' , to:'sessions#new'
+  post'/login', to:'sessions#create'
+  delete'/logout',to:'sessions#destroy'
+  get'/signup' , to:'users#new'
 end
