bbfeda0b76bb2bdd6cc3ec0224f543173b046c8e
Ruchelle Balasuriya <rushebala@gmail.com>
2020-05-18 20:44:25 +1000

    micropost

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
 gem 'rails-ujs', '~> 0.1.0'
 gem 'bcrypt','3.1.12'
 gem 'bootstrap-sass', '3.3.7'
+gem 'rails-controller-testing'
 # Use postgresql as the database for Active Record
 # gem 'pg', '>= 0.18', '< 2.0'
 group :production do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action:logged_in_user,only:[:index,:edit,:update]
+  before_action:correct_user,only:[:edit,:update]
+
+  def index
+  end
+
   def show
     @user = User.find(params[:id]) 
+    # @microposts=@user.microposts.paginate(page: params[:page])
   end
   def new
     @user=User.new
@@ -15,7 +22,24 @@ class UsersController < ApplicationController
       render 'new'     
     end   
   end
+
+  private
+
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation,:mobile)
   end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger]="Please log in."
+      redirect_to login_url
+    end
+  end
+  
+def correct_user
+  @user=User.find(params[:id])
+  redirect_to(root_url)unless current_user?(@user)
+end
+
 end
