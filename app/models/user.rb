9d57cff980b85afd7b6dd2b427b9ced9781089f2
Si Long Leong s3785099 <s3785099@student.rmit.edu.au>
2020-05-30 11:59:29 +1000

    forget passowrd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :reset_token
     has_many :comments
     has_many :microposts
     default_scope -> { order(last_seen_at: :desc) }
@@ -22,6 +22,20 @@ class User < ApplicationRecord
         SecureRandom.urlsafe_base64
     end
 
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token=User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+        # update_columns(reset_digest :FILL_IN, reset_sent_at :FILL_IN)
+
+    end
+    
+    # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
     def remember
         self.remember_token=User.new_token
         update_attribute(:remember_digest,User.digest(remember_token))
@@ -31,6 +45,11 @@ class User < ApplicationRecord
         return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
    end
+   
+   def password_reset_expired?
+    reset_sent_at<2.hours.ago
+    end
+
    def forget
     update_attribute(:remember_digest,nil)
   end
